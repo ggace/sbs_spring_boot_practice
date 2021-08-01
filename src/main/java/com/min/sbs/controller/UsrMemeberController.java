bc17b4f4fff54f4baf37a4987cfd4f54d496c3ff
ggace <skydream5485@gmail.com>
2021-08-01 20:39:10 +0900

    doJoin에 ResultData 적용

--- src/main/java/com/min/sbs/controller/UsrMemeberController.java
+++ src/main/java/com/min/sbs/controller/UsrMemeberController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.min.sbs.dto.Member;
+import com.min.sbs.dto.ResultData;
 import com.min.sbs.service.MemberService;
 import com.min.sbs.util.Util;
 
@@ -18,42 +19,44 @@ public class UsrMemeberController {
 
 	@RequestMapping("usr/member/doJoin")
 	@ResponseBody
-	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	public ResultData doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
 
-		if (!Util.isEmpty(loginId)) {
-			return "loginId를 입력해주세요";
+		if (Util.isEmpty(loginId)) {
+			return ResultData.from("F-A", "loginId를 입력해주세요");
 		}
 
-		if (!Util.isEmpty(loginPw)) {
-			return "loginPw를 입력해주세요";
+		if (Util.isEmpty(loginPw)) {
+			return ResultData.from("F-B", "loginPw를 입력해주세요");
 		}
 
-		if (!Util.isEmpty(name)) {
-			return "name를 입력해주세요";
+		if (Util.isEmpty(name)) {
+			return ResultData.from("F-C", "name를 입력해주세요");
 		}
 
-		if (!Util.isEmpty(nickname)) {
-			return "nickname를 입력해주세요";
+		if (Util.isEmpty(nickname)) {
+			return ResultData.from("F-D", "nickname를 입력해주세요");
 		}
 
-		if (!Util.isEmpty(cellphoneNo)) {
-			return "cellphoneNo를 입력해주세요";
+		if (Util.isEmpty(cellphoneNo)) {
+			return ResultData.from("F-E", "cellphoneNo를 입력해주세요");
 		}
 
-		if (!Util.isEmpty(email)) {
-			return "email를 입력해주세요";
+		if (Util.isEmpty(email)) {
+			return ResultData.from("F-F", "email를 입력해주세요");
 		}
 
-		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-
-		if (id == -1) {
-			return Util.format("해당 아이디(%s)는 이미 사용중입니다.", loginId);
-		} else if (id == -2) {
-			return Util.format("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.", name, email);
+		ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+		
+		if(joinRd.isFail()) {
+			return joinRd;
 		}
+		
+		int id = (int)joinRd.getData();
+		
+		Member member = memberService.getMemberById(id);
 
-		return memberService.getMemberById(id);
+		return ResultData.newData(joinRd, member);
 
 	}
 }
--- src/main/java/com/min/sbs/service/MemberService.java
+++ src/main/java/com/min/sbs/service/MemberService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 import com.min.sbs.dao.MemberDao;
 import com.min.sbs.dto.Member;
+import com.min.sbs.dto.ResultData;
+import com.min.sbs.util.Util;
 
 @Service
 public class MemberService {
@@ -13,22 +15,22 @@ public class MemberService {
 		this.memberDao = memberRepository;
 	}
 
-	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+	public ResultData join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 
 		Member oldMember = memberDao.getMemberByLoginId(loginId);
 
 		if (oldMember != null) {
-			return -1;
+			return ResultData.from("F-1", Util.format("해당 아이디(%s)는 이미 사용중입니다.", loginId));
 		}
 		oldMember = memberDao.getMemberByNameAndEmail(name, email);
 
 		if (oldMember != null) {
-			return -2;
+			return ResultData.from("F-2", Util.format("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.", name, email));
 		}
 
 		memberDao.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-
-		return memberDao.getLastId();
+		
+		return ResultData.from("S-1", "회원 가입되었습니다.", memberDao.getLastId());
 	}
 
 	public Member getMemberById(int id) {
