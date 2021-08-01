d3026703dfb3ac833feea20a84220b60e10fc68f
ggace <skydream5485@gmail.com>
2021-08-01 19:59:44 +0900

    ResultData 구현

---
+++ src/main/java/com/min/sbs/dto/ResultData.java
@@ -0,0 +1,33 @@
+package com.min.sbs.dto;
+
+import lombok.Getter;
+
+public class ResultData {
+	@Getter
+	private String resultCode;
+	@Getter
+	private String msg;
+	@Getter
+	private Object data;
+
+	private ResultData() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static ResultData from(String resultCode, String msg, Object data) {
+		ResultData resultData = new ResultData();
+		resultData.resultCode = resultCode;
+		resultData.msg = msg;
+		resultData.data = data;
+
+		return resultData;
+	}
+
+	public boolean isSuccess() {
+		return resultCode.startsWith("S-");
+	}
+
+	public boolean isFail() {
+		return !isSuccess();
+	}
+}
